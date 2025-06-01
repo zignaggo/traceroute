bd346b34fab156ffd288da45901477f549d209e7
qualifica <qualiificatech@gmail.com>
2025-06-01 01:50:35 -0300

    feat: integrate TanStack Router for improved routing and navigation, add route tree generation, and implement root route component

---
+++ src/routes/__root.tsx
@@ -0,0 +1,35 @@
+import { createRootRoute } from "@tanstack/react-router";
+import { Flow } from "@/components/flow";
+import { Header } from "@/components/header";
+import { Tabs, TabsContent } from "@/components/ui/tabs";
+import { Events } from "@/components/events";
+import { Listener } from "@/components/listener";
+import { Terminal } from "@/components/terminal";
+import "@/index.css";
+import { Titlebar } from "@/components/titlebar";
+import { cn } from "@/lib/utils";
+import { theme } from "@/store";
+export const Route = createRootRoute({
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <main className={cn("h-screen bg-secondary flex flex-col", theme.value === "dark" && "dark")}>
+      <Titlebar />
+      <Listener />
+      <Tabs defaultValue="flow" className="flex w-full flex-1 gap-0 overflow-auto">
+        <Header />
+        <TabsContent value="flow" className="flex-1 overflow-auto">
+          <Flow />
+        </TabsContent>
+        <TabsContent value="terminal" className="flex-1 overflow-auto bg-background">
+          <Terminal />
+        </TabsContent>
+        <TabsContent value="events" className="flex-1 overflow-auto">
+          <Events />
+        </TabsContent>
+      </Tabs>
+    </main>
+  );
+}
